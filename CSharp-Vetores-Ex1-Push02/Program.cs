0a4331717a19c308c96b4646581cd4f7f5bcda70
Lethícia Santos <leticia-santos75@hotmail.com>
2022-02-17 19:15:31 -0300

    Adicionar arquivos de projeto.

---
+++ CSharp-Vetores-Ex1-Push02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Vetores_Ex1_Push02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
